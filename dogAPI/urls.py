77abaeee988587d89dcbe96e54282025e8344f6d
smithrunner <smithrunner@live.com>
2022-11-19 22:46:13 -0600

    contiuned work on api endpoints

--- dogAPI/urls.py
+++ dogAPI/urls.py
@@ -3,5 +3,7 @@ from dogAPI import views
 
 urlpatterns = [
     re_path(r'^api/keys$', views.key_list),
-    re_path(r'^api/keys/(?P<pk>[a-zA-Z0-9]+)$', views.key_detail)
+    re_path(r'^api/keys/(?P<pk>[a-zA-Z0-9]+)$', views.key_detail),
+    re_path(r'^api/dogs$', views.dog_grab),
+    re_path(r'^api/dogs/(?P<pk>[0-9]+)$', views.dog_display)
 ]
